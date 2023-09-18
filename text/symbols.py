161fa58de0c48b5e3e399c97ca020d7823292a3d
nicoloddo <gnicoloddo@gmail.com>
2023-09-18 14:51:46 +0200

    More disfluencies labeling, differentiate breath and long_breath

--- text/symbols.py
+++ text/symbols.py
@@ -3,17 +3,21 @@
 '''
 Defines the set of symbols used in text input to the model.
 '''
-DISFLUENCIES = ['uh', 'ah', 'eh', 'oh', 'um', 'em']
+DISFLUENCIES = [
+    "ah", "eh", "mm", "oo", "uh", "er", "um", "oh", 
+    "ooh", "eeh", "ohh", "ehh", "ahh", "hum", "huh", "mhm", "erm", "huh", "shh", "uhm", "ehm", "uhh", "hmm",
+    "oops", "phew", "psst", "yoo-hoo", "zing", "yikes", "uh-huh", "ouch", "tsk-tsk", "uh-oh", "ahem", "mmhmm"
+]
 
 _pad        = '_'
 _punctuation = ';:,.!?¡¿—…"«»“” '
 _letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 _letters_ipa = "ɑɐɒæɓʙβɔɕçɗɖðʤəɘɚɛɜɝɞɟʄɡɠɢʛɦɧħɥʜɨɪʝɭɬɫɮʟɱɯɰŋɳɲɴøɵɸθœɶʘɹɺɾɻʀʁɽʂʃʈʧʉʊʋⱱʌɣɤʍχʎʏʑʐʒʔʡʕʢǀǁǂǃˈˌːˑʼʴʰʱʲʷˠˤ˞↓↑→↗↘'̩'ᵻ"
-_breath = '<breath>'
+_breaths = ['<breath>', '<long_breath'>
 _disfluencies = ['<disfl.' + disfl.lower() + '>' for disfl in DISFLUENCIES]
 
 # Export all symbols:
-symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa) + [_breath] + _disfluencies
+symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa) + _breaths + _disfluencies
 
 # Special symbol ids
 SPACE_ID = symbols.index(" ")
